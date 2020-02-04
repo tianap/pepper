55910cf93aec647b5a5b4391cd0ee4039a81d185
Kishwar <kishwar.shafin@gmail.com>
2020-02-04 15:31:54 -0800

    manual seed turned off :fire:

--- modules/python/models/predict_distributed_gpu.py
+++ modules/python/models/predict_distributed_gpu.py
@@ -115,11 +115,11 @@ def setup(rank, total_threads, args, all_input_files):
     filepath, output_filepath, model_path, batch_size, num_workers = args
 
     # issue with semaphore lock: https://github.com/pytorch/pytorch/issues/2517
-    mp.set_start_method('spawn')
+    # mp.set_start_method('spawn')
 
     # Explicitly setting seed to make sure that models created in two processes
-    # start from same random weights and biases.
-    torch.manual_seed(42)
+    # start from same random weights and biases. https://github.com/pytorch/pytorch/issues/2517
+    # torch.manual_seed(42)
     predict(filepath, all_input_files[rank],  output_filepath, model_path, batch_size, num_workers, rank)
     cleanup()
 
