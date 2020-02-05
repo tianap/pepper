04d76aaf8f5fa0f941730598e04a5938ad0b0579
Kishwar <kishwar.shafin@gmail.com>
2020-02-04 16:31:34 -0800

    tqdm issues :fire:

--- modules/python/models/predict_distributed_gpu.py
+++ modules/python/models/predict_distributed_gpu.py
@@ -98,9 +98,8 @@ def predict(input_filepath, file_chunks, output_filepath, model_path, batch_size
                 prediction_data_file.write_prediction(contig[i], contig_start[i], contig_end[i], chunk_id[i],
                                                       position[i], index[i], prediction_base_tensor[i], ref_seq[i])
             progress_bar.update(1)
-
-    progress_bar.clear()
     progress_bar.close()
+    del progress_bar
 
 
 def cleanup():
